30b5f95cf65b13e654d8cd68e4c1e858f25b5449
Richard Cho <richardcho100@gmail.com>
2018-04-18 03:00:08 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ hash = { :ashley => 2, :blake => 500, :adam => 1}
 
 require "pry"
 def key_for_min_value(name_hash)
-  min_name = ""
+  min_name = nil 
   min_number = ""
   name_hash.each do |name, number|
     if min_number == ""
